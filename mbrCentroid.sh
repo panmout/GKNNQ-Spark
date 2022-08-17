c82839e83071f7bc2a4ec54251250a909bff28f7
panmout <takismoutafis@outlook.com>
2022-08-17 22:26:41 +0300

    renamed project and added github in pom

--- mbrCentroid.sh
+++ mbrCentroid.sh
@@ -1,6 +1,6 @@
 # execute (<namenode name> <hdfs dir name> <query dataset> <hdfs GNN dir name> <step> <mindist> <counter_limit> <pointdist>)
 
-hadoop jar ./target/spark-0.0.1-SNAPSHOT.jar gr.uth.ece.dsel.spark.preliminary.MbrCentroid \
+hadoop jar ./target/gknn-spark-0.0.1-SNAPSHOT.jar gr.uth.ece.dsel.spark.preliminary.MbrCentroid \
 nameNode=Hadoopmaster \
 queryDir=input \
 queryDataset=linearwaterNNew_sub_2.8M.txt \
